f2f8f31dc20d618afa913a167295a82486a66d78
anhdaijka <anhdaijka@gmail.com>
2026-02-28 05:26:51 +0700

    fix: ui auth page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import { AppLogo } from "@/components/shared/AppLogo";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="flex flex-col min-h-screen items-center justify-center">
-			<div className="mb-8 pt-8">
-				<AppLogo imageClassName="h-16 w-auto" />
-			</div>
 			{children}
 		</div>
 	);
